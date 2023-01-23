31aa45a13dc8ba3be9b7faeb04c04ff4da9ad86d
MartinKotas <kotasmartin1997@gmail.com>
2023-01-23 22:41:51 +0000

    somewhat things done

---
+++ src/api/users/index.js
@@ -0,0 +1,160 @@
+import express from "express";
+import createHttpError from "http-errors";
+import UserModel from "./model.js";
+import multer from "multer";
+import { pipeline } from "stream";
+/*
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+*/
+const usersRouter = express.Router();
+
+usersRouter.post("/", async (req, res, next) => {
+  try {
+    const newUser = new UserModel(req.body);
+    const { _id } = await newUser.save();
+    res.status(201).send({ _id });
+  } catch (error) {
+    console.log(error);
+    createHttpError(
+      400,
+      "The username ${req.params.username} is already taken, Please choose a different name"
+    );
+    res.status(400).send(error);
+    next(error);
+  }
+});
+
+usersRouter.get("/", async (req, res, next) => {
+  try {
+    const users = await UserModel.find();
+    res.send(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.get("/:userId", async (req, res, next) => {
+  try {
+    const user = await UserModel.findById(req.params.userId);
+    if (user) {
+      res.send(user);
+    } else {
+      next(
+        createHttpError(404, `user with id ${req.params.userId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.put("/:userId", async (req, res, next) => {
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      req.params.userId,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (updatedUser) {
+      res.send(updatedUser);
+    } else {
+      next(
+        createHttpError(404, `User with id ${req.params.userId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.delete("/:userId", async (req, res, next) => {
+  try {
+    const deletedUser = await UserModel.findByIdAndDelete(req.params.userId);
+    if (deletedUser) {
+      res.status(204).send("successfully deleted");
+    } else {
+      next(
+        createHttpError(404, `User with id ${req.params.userId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//Profile picture
+/*
+const cloudinaryUploader = multer({
+  storage: new CloudinaryStorage({
+    cloudinary,
+    params: {
+      folder: "UserProfilePhotos",
+    },
+  }),
+}).single("profilePic");
+
+usersRouter.post(
+  "/:userId/profilePic",
+  cloudinaryUploader,
+  async (req, res, next) => {
+    try {
+      console.log(req.file);
+      const url = req.file.path;
+      //find the user
+      const oldUser = await UserModel.findById(req.params.userId);
+      console.log("2");
+      //add new image url to ... I am having trouble overwriting the data that was there
+      const updatePic = { oldUser, image: url };
+      console.log("3");
+      const updatedUser = updatePic;
+      console.log(updatedUser);
+      console.log("4");
+      res.status(201).send(updatedUser._doc);
+    } catch (error) {
+      res.send(error);
+      next(error);
+    }
+  }
+);
+*/
+//and PDF
+
+// Epreriences embedded
+usersRouter.get("/:userId/experiences", async (req, res, next) => {
+  try {
+    const user = await UserModel.findById(req.params.userId);
+    console.log(user);
+    if (user) {
+      res.send(user.experiences);
+    } else {
+      next(
+        createHttpError(404, `User with id ${req.params.userId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.post("/:userId/experiences", async (req, res, next) => {
+  try {
+    const newExperience = { ...req.body };
+    if (newExperience) {
+      const updatedUser = await UserModel.findByIdAndUpdate(
+        req.params.userId,
+        { $push: { experiences: newExperience } },
+        { new: true, runValidators: true }
+      );
+      res.send(updatedUser);
+    } else {
+      next(
+        createHttpError(404, `User with id ${req.params.userId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default usersRouter;
---
+++ src/api/users/model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import mongooseUniqueValidator from "mongoose-unique-validator";
+
+const { Schema, model } = mongoose;
+
+const experiencesSchema = new Schema(
+  {
+    role: { type: String, required: true },
+    company: { type: String, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: false },
+    description: { type: String, required: true },
+    area: { type: String, required: true },
+    image: {
+      type: String,
+      default:
+        "https://www.labrador-owners.co.uk/images/user_images//system/default.jpg?v=1632230965",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const usersSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    email: { type: String, required: true, uniqueCaseInsensitive: true },
+    bio: { type: String, required: true },
+    title: { type: String, required: true },
+    area: { type: String, required: true },
+    image: {
+      type: String,
+      required: true,
+      default:
+        "https://www.labrador-owners.co.uk/images/user_images//system/default.jpg?v=1632230965",
+    },
+    username: { type: String, required: true, unique: true },
+    experiences: [experiencesSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+usersSchema.plugin(mongooseUniqueValidator, {
+  message: "Error, expected userName to be unique.",
+});
+
+export default model("User", usersSchema);
