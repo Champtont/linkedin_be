2713e9302306baf4e53e929561fed1b27fed3a3c
Courtney Hampton <jaye0888@gmail.com>
2023-01-24 18:55:06 +0100

    Updated posts schema
    The users array already receives a user name and the user is referenced in the post schema. Also, the image uploaded to the server must be a default image.

--- src/api/posts/model.js
+++ src/api/posts/model.js
@@ -5,8 +5,11 @@ const { Schema, model } = mongoose;
 const postsSchema = new Schema(
   {
     text: { type: String, required: true },
-    username: { type: String, required: true },
-    image: { type: String, required: true },
+    image: {
+      type: String,
+      required: true,
+      default: "https://picsum.photos/600",
+    },
     user: { type: Schema.Types.ObjectId, ref: "User" },
   },
 
